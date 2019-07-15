7dba109e9d9f977fad37226f7bb8f30533917814
Robert Dannelly <robertdannelly@gmail.com>
2019-07-15 17:00:51 -0400

    Update all Questions for Microsoft

---
+++ Mystery Method/MysteryMethod.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystery_Method
+{
+    public class MystryMethod
+    {
+        public static void Main(string[] args)
+        {
+            // Populate Person class with name and aquatiances
+            Person p = new Person();
+            
+            p.Name = "Robert";
+            p.Acquaintances. = { p.Name = "Tom", p.Name="Tommy"};
+            
+
+
+
+
+
+
+
+        }
+
+        //3. Mystery Method               
+        //Describe what the Mystery method does and discuss any 
+        //potential bugs and possible fixes.
+
+        /// <summary>
+        /// public class P accessesible to anyone who has access to the .exe
+        /// </summary>
+        public class Person
+        {
+            public string Name;
+            public Person[] Acquaintances;
+
+            public Person()
+            {
+
+            }
+
+            public Person(string name, Person[] acquaintances)
+            {
+                if (IsNameNullOrWhiteSpace(name)) //RBD change to call Mystery
+                {
+                    throw new ArgumentException("Name cannot be null o white space.",
+                     "name");
+                }
+
+                this.Name = name;
+                this.Acquaintances = acquaintances;
+            }
+
+            public bool IsNameNullOrWhiteSpace(string name)
+            {
+                if (String.IsNullOrWhiteSpace(name))
+                {
+                    return true;
+                }
+
+                Stack<Person> myStack = new Stack<Person>();
+                foreach (Person acquaintance in this.Acquaintances)
+                {
+                    myStack.Push(acquaintance);
+                }
+
+                do
+                {
+                    var person = myStack.Pop();
+
+                    if (person.Name.Equals(name))
+                    {
+                        return true;
+                    }
+
+                    foreach (Person acquaintance in person.Acquaintances)
+                    {
+                        myStack.Push(acquaintance);
+                    }
+
+                } while (myStack.Count >= 0);
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ SortProductsbyPriority/SortProduct.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortProductsbyPriority
+{
+    //    Products are identified by alphanumeric codes.Each code is stored as a 
+    //string. We have three types of products: high priority, medium priority, and 
+    //low priority.Given an array of product codes, sort the array so that the highest 
+    //priority products come at the beginning of the array, the medium priority 
+    //products come in the middle, and the low priority products come at the end.
+
+    //Within a priority group, order does not matter. You are given a priority function which, given a 
+    //product code, 
+    //HINT ENUM returns 1 for high, 2 for medium and 3 for low.This array may contain a 
+    //large number of product codes, so do your best to minimize additional storage.
+
+    //You are given this function for usage:
+    //private int GetPriority(string productCode).
+    //You don’t need to implement this function.
+    //Please Implement:
+    //public void OrderProductsByPriority(string[] productCodes)
+
+
+
+    public class SortProduct
+    {
+        public static void Main(string[] args)
+        {
+            Products product1 = new Products();
+            product1.productCodes = "12345";
+            product1.productPriority = Priority.High;
+
+            Products product2 = new Products();
+            product1.productCodes = "12444";
+            product1.productPriority = Priority.Medum;
+
+            Products product3 = new Products();
+            product1.productCodes = "12555";
+            product1.productPriority = Priority.High;
+
+            Products product4 = new Products();
+            product1.productCodes = "12355";
+            product1.productPriority = Priority.Low;
+
+            Products product5 = new Products();
+            product1.productCodes = "12347";
+            product1.productPriority = Priority.Low;
+
+
+
+            Products[] products = new Products[4];
+            products[2] = products[2];
+            products[4] = products[3];
+            products[5] = products[4];
+            products[5] = products[4];
+
+
+
+            products = OrderProductsByPriority(Products products);
+
+
+        }
+
+        public List<Products> OrderProductsByPriority(Products products)
+        {
+            products
+
+
+
+            return products;
+
+        }
+    }
+
+    public enum Priority{
+        High = 1,
+        Medum,
+        Low
+    }
+
+    public class Products
+    {
+        public string productCodes { get; set; }
+        public Priority productPriority { get; set; }
+
+    }
+
+
+
+
+
+}
